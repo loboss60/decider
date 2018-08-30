b764734b1547d1ea047d498aca519024c2958bd7
Your Name <you@example.com>
2018-08-30 14:39:55 +0200

    Both task(todo) and duration is in one array element, showing it on the logcat. Later I will parse them and extract foirs list element (todo) and again get random element from the rest of the array

--- app/src/main/java/com/example/jasenkokrejic/decider/MainActivity.java
+++ app/src/main/java/com/example/jasenkokrejic/decider/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jasenkokrejic.decider;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -14,11 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Log.i("jasenko","jasenko");
         Button button=(Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,showtime.class);
+//                Log.i("myTag", "hhhhh");
+//                Log.i("myTag","hhhhhhhhh");
                 startActivity(intent);
 
 
--- app/src/main/java/com/example/jasenkokrejic/decider/showtime.java
+++ app/src/main/java/com/example/jasenkokrejic/decider/showtime.java
@@ -2,6 +2,11 @@ package com.example.jasenkokrejic.decider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class showtime extends AppCompatActivity {
 
@@ -11,9 +16,16 @@ public class showtime extends AppCompatActivity {
         setContentView(R.layout.activity_showtime);
         Integer  broj=5;
         String[] todo = new String[broj];
-        todo[0] = "Prošetati";
-        todo[1] = "LAMP Server";
+        todo[0] = "Prošetati,5,10";
+        todo[1] = "LAMP Server,10,15,20";
+        todo[2] = "GIT casual reading,10,15,20";
+        todo[3] = "PostgreSQL,10,15";
+        todo[4] = "LAMP Server,15,20,25";
+        Integer randy=new Random().nextInt(todo.length);
+        String todox = todo[randy];
+        List<String> result = Arrays.asList(todox.split("\\s*,\\s*"));
+        Log.i("myTag",result.toString());
 
-        Integer[] duration=new Integer[broj];
+        Log.i("myTag",todox);
     }
 }
