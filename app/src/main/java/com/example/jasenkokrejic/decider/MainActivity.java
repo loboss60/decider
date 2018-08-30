a82bc03df98a294d7b335f931b8d4e147c3ffa64
Your Name <you@example.com>
2018-08-30 13:44:03 +0200

    Added onclick to button that takes us to the showtime activity

--- app/src/main/java/com/example/jasenkokrejic/decider/MainActivity.java
+++ app/src/main/java/com/example/jasenkokrejic/decider/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.jasenkokrejic.decider;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,showtime.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
 
     }
 }
---
+++ app/src/main/java/com/example/jasenkokrejic/decider/showtime.java
@@ -0,0 +1,13 @@
+package com.example.jasenkokrejic.decider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class showtime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showtime);
+    }
+}
